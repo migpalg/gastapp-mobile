58693d432e37ac45a9d3ae4b264c31c11e9b2935
Miguel Palacio <migpalg@gmail.com>
2020-01-30 15:10:43 -0500

    Scaffolding HTTP

--- src/providers/http.ts
+++ src/providers/http.ts
@@ -1,5 +1,14 @@
 class HttpClient {
-  private makeRequest = (url: string, init?: RequestInit) => fetch(url, init);
+  private makeRequest = (url: string, init?: RequestInit) =>
+    fetch(url, init).then(response => response.json());
 }
 
-export default new HttpClient();
+abstract class ClientWrapper {
+  // Single instance of HTTP Client
+  private static http = new HttpClient();
+
+  // Root url to make requests
+  abstract rootUrl: string;
+}
+
+export default ClientWrapper;
