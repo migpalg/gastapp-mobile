c330d9d74648f7f292139f68e32e6baebb4ebf0d
Miguel Palacio <migpalg@gmail.com>
2020-01-30 16:39:17 -0500

    Scaffold again HTTP

--- src/providers/http.ts
+++ src/providers/http.ts
@@ -1,11 +1,18 @@
-class HttpClient {
+/**
+ * Wraps all HTTP methods, GET, POST, etc.
+ * This manages tokens on cookies
+ */
+class BaseHttp {
   private makeRequest = (url: string, init?: RequestInit) =>
     fetch(url, init).then(response => response.json());
 }
 
+/**
+ * Wraps a single instance of HTTP Client
+ */
 abstract class ClientWrapper {
   // Single instance of HTTP Client
-  private static http = new HttpClient();
+  private static http = new BaseHttp();
 
   // Root url to make requests
   abstract rootUrl: string;
